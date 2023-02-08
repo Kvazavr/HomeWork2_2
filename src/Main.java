5c3f6893a0a8864f89445e8667ca5dbb87d07e94
Elena Nekrasova <kvazavr@yandex.ru>
2023-02-08 11:03:43 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int start = 700;
+        int deposit = 1000;
+        int bonus = deposit/100;
+        if (deposit < 1000) {
+            System.out.println("На Вашем балансе " + (start+deposit) + " рублей");
+        } else {
+            System.out.println("На Вашем балансе " + (start+deposit+bonus) + " рублей," + " в том числе " + bonus + " бонусных баллов");
+        }
+    }
+}
