252f63c494fbc9cb49e1bf6f98e7c562aebf6de6
Elena Nekrasova <kvazavr@yandex.ru>
2023-02-10 11:58:44 +0400

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         int start = 700;
         int deposit = 1000;
-        int bonus = deposit/100;
+        int bonus = deposit / 100;
         if (deposit < 1000) {
-            System.out.println("На Вашем балансе " + (start+deposit) + " рублей");
+            System.out.println("На Вашем балансе " + (start + deposit) + " рублей");
         } else {
-            System.out.println("На Вашем балансе " + (start+deposit+bonus) + " рублей," + " в том числе " + bonus + " бонусных баллов");
+            System.out.println("На Вашем балансе " + (start + deposit + bonus) + " рублей," + " в том числе " + bonus + " бонусных баллов");
         }
     }
 }
